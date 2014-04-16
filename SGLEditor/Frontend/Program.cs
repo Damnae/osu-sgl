043bc3ec523388179fab41588b6c7297608dfbf5
Damnae <damnaee@gmail.com>
2014-04-16 20:37:12 +0200

    Remove some unused things in the editor

--- SGLEditor/Frontend/Program.cs
+++ SGLEditor/Frontend/Program.cs
@@ -1,23 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
-using SGLTest;
 
-namespace WindowsFormsApplication4
-{
-    static class Program
-    {
-        /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SimpleSGLEditor());
-            //Application.Run(new Form1());
-            //Application.Run(new Kana_finden());
-        }
-    }
+namespace SGLEditor {
+	static class Program {
+		/// <summary>
+		/// Der Haupteinstiegspunkt für die Anwendung.
+		/// </summary>
+		[STAThread]
+		static void Main() {
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new SimpleSGLEditor());
+		}
+	}
 }
