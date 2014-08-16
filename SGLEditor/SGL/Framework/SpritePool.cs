75b415d9b1be75274d7b7e2eef285fc9f8238f50
Damnae <damnaee@gmail.com>
2014-08-16 13:52:50 +0200

    Utility classes to reuse sprites

---
+++ SGLEditor/SGL/Framework/SpritePool.cs
@@ -0,0 +1,50 @@
+﻿using SGL.Storyboard.Generators.Visual;
+using System;
+using System.Collections.Generic;
+
+namespace SGL.Framework {
+	public class SpritePool {
+		private string path;
+		private string layer;
+		private string origin;
+
+		private List<PooledSprite> pooledSprites = new List<PooledSprite>();
+
+		public SpritePool(String path, String layer, String origin) {
+			this.path = path;
+			this.layer = layer;
+			this.origin = origin;
+		}
+
+		public SpriteGenerator Get(double startTime) {
+			foreach (var pooledSprite in pooledSprites) {
+				if (pooledSprite.endTime < startTime) {
+					pooledSprites.Remove(pooledSprite);
+					return pooledSprite.sprite;
+				}
+			}
+
+			return SB.Sprite(path, layer, origin);
+		}
+		
+		public SpriteGenerator Get(double startTime, double endTime) {
+			var sprite = Get(startTime);
+			Release(sprite, endTime);
+			return sprite;
+		}
+
+		public void Release(SpriteGenerator sprite, double endTime) {
+			pooledSprites.Add(new PooledSprite(sprite, endTime));
+		}
+
+		class PooledSprite {
+			public SpriteGenerator sprite;
+			public double endTime;
+
+			public PooledSprite(SpriteGenerator sprite, double endTime) {
+				this.sprite = sprite;
+				this.endTime = endTime;
+			}
+		}
+	}
+}
---
+++ SGLEditor/SGL/Framework/SpritePools.cs
@@ -0,0 +1,41 @@
+﻿using SGL.Storyboard.Generators.Visual;
+using System;
+using System.Collections.Generic;
+
+namespace SGL.Framework {
+	public class SpritePools {
+		private Dictionary<String, SpritePool> pools = new Dictionary<String, SpritePool>();
+
+		public SpriteGenerator Get(double startTime, double endTime, String path, String layer, String origin) {
+			return GetPool(path, layer, origin).Get(startTime, endTime);
+		}
+
+		public SpriteGenerator Get(double startTime, String path, String layer, String origin) {
+			return GetPool(path, layer, origin).Get(startTime);
+		}
+
+		public void Release(SpriteGenerator sprite, double endTime) {
+			GetPool(sprite.Filepath, sprite.Layer, sprite.Origin).Release(sprite, endTime);
+		}
+
+		public void Clear() {
+			pools.Clear();
+		}
+		
+		private SpritePool GetPool(String path, String layer, String origin) {
+			String key = GetKey(path, layer, origin);
+
+			SpritePool pool;
+			if (!pools.TryGetValue(key, out pool)) {
+				pool = new SpritePool(path, layer, origin);
+				pools.Add(key, pool);
+			}
+
+			return pool;
+		}
+
+		private String GetKey(String path, String layer, String origin) {
+			return path + "#" + layer + "#" + origin;
+		}
+	}
+}
