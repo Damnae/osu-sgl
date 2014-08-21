14b870811b03d338164f47aa520869cea7049814
Damnae <damnaee@gmail.com>
2014-08-21 15:56:30 +0200

    Add a group parameter to avoid reusing sprites with different commands types

--- SGLEditor/SGL/Framework/SpritePools.cs
+++ SGLEditor/SGL/Framework/SpritePools.cs
@@ -6,20 +6,28 @@ namespace SGL.Framework {
 	public class SpritePools {
 		private Dictionary<String, SpritePool> pools = new Dictionary<String, SpritePool>();
 
+		public SpriteGenerator Get(double startTime, double endTime, String path, String layer, String origin, bool additive, int group) {
+			return GetPool(path, layer, origin, additive, group).Get(startTime, endTime);
+		}
+
 		public SpriteGenerator Get(double startTime, double endTime, String path, String layer, String origin, bool additive) {
-			return GetPool(path, layer, origin, additive).Get(startTime, endTime);
+			return GetPool(path, layer, origin, additive, 0).Get(startTime, endTime);
+		}
+
+		public SpriteGenerator Get(double startTime, double endTime, String path, String layer, String origin, int group) {
+			return GetPool(path, layer, origin, false, group).Get(startTime, endTime);
 		}
 
 		public SpriteGenerator Get(double startTime, double endTime, String path, String layer, String origin) {
-			return GetPool(path, layer, origin, false).Get(startTime, endTime);
+			return GetPool(path, layer, origin, false, 0).Get(startTime, endTime);
 		}
 
 		public SpriteGenerator Get(double startTime, String path, String layer, String origin) {
-			return GetPool(path, layer, origin, false).Get(startTime);
+			return GetPool(path, layer, origin, false, 0).Get(startTime);
 		}
 
 		public void Release(SpriteGenerator sprite, double endTime) {
-			GetPool(sprite.Filepath, sprite.Layer, sprite.Origin, false).Release(sprite, endTime);
+			GetPool(sprite.Filepath, sprite.Layer, sprite.Origin, false, 0).Release(sprite, endTime);
 		}
 
 		public void Clear() {
@@ -28,8 +36,8 @@ namespace SGL.Framework {
 			pools.Clear();
 		}
 
-		private SpritePool GetPool(String path, String layer, String origin, bool additive) {
-			String key = GetKey(path, layer, origin, additive);
+		private SpritePool GetPool(String path, String layer, String origin, bool additive, int group) {
+			String key = GetKey(path, layer, origin, additive, group);
 
 			SpritePool pool;
 			if (!pools.TryGetValue(key, out pool)) {
@@ -40,8 +48,8 @@ namespace SGL.Framework {
 			return pool;
 		}
 
-		private String GetKey(String path, String layer, String origin, bool additive) {
-			return path + "#" + layer + "#" + origin + "#" + (additive ? "1" : "0");
+		private String GetKey(String path, String layer, String origin, bool additive, int group) {
+			return path + "#" + layer + "#" + origin + "#" + (additive ? "1" : "0") + "#" + group;
 		}
 	}
 }
